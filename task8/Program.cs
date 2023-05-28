bc2c2cf2ec8110d3c4c93086fe0984aa750407e5
Louisa <131443655+Severylol@users.noreply.github.com>
2023-05-28 08:20:15 +0300

    Update Program.cs

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Clear();
 {
 Console.Write("Введите число: ");
-    int N = Convert.ToInt32(Console.ReadLine());
+    int N = Convert.ToInt32(Console.ReadLine()!);
 Console.WriteLine("Чётные числа от 1 до " + N + ":");
 for (int i = 2; i <= N; i += 2)
 {
